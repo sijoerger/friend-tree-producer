0ce18251d6a64d935a3e53567ee61426ef6d4c15
Sebastian Wozniewski <sebastian.wozniewski@student.kit.edu>
2019-05-23 10:53:03 +0200

    add database update script

---
+++ scripts/update_input_params.sh
@@ -0,0 +1,6 @@
+cd $CMSSW_BASE/src/HiggsAnalysis/friend-tree-producer/data/input_params
+read -p "lxplus-username: " USERNMLXP
+scp ${USERNMLXP}@lxplus.cern.ch:/eos/home-s/swozniew/friend-tree-producer-input-params/* $CMSSW_BASE/src/HiggsAnalysis/friend-tree-producer/data/input_params/
+rm datasets.json
+wget https://raw.githubusercontent.com/KIT-CMS/Kappa/dictchanges/Skimming/data/datasets.json
+
