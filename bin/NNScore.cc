cac2906824f065f00d2e97e6c30b8ea04ecf7c5c
Artur Gottmann <artur.akhmetshin@cern.ch>
2019-05-28 16:27:51 +0200

    bin/NNScore.cc: adding first draft for NNScore calculation for SM HTT ML analysis

---
+++ bin/NNScore.cc
@@ -0,0 +1,125 @@
+#include "lwtnn/LightweightNeuralNetwork.hh"
+#include "lwtnn/parse_json.hh"
+#include <fstream>
+
+#include "TFile.h"
+#include "TH1F.h"
+#include "TTree.h"
+#include "TVector2.h"
+#include "Math/LorentzVector.h"
+#include "Math/PtEtaPhiM4D.h"
+#include "Math/PxPyPzM4D.h"
+#include "Math/Vector4Dfwd.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+#include <boost/regex.hpp>
+
+#include <iostream>
+
+#include "HiggsAnalysis/friend-tree-producer/interface/HelperFunctions.h"
+
+using boost::starts_with;
+namespace po = boost::program_options;
+
+int main(int argc, char **argv) {
+  std::string input = "output.root";
+  std::vector<std::string> input_friends  = {};
+  std::string folder = "mt_nominal";
+  std::string tree = "ntuple";
+  std::string lwtnn_config = "fold0_keras_lwtnn.json";
+  unsigned int first_entry = 0;
+  unsigned int last_entry = 9;
+  po::variables_map vm;
+  po::options_description config("configuration");
+  config.add_options()
+     ("input",         po::value<std::string>(&input)->default_value(input))
+     ("input_friends", po::value<std::vector<std::string>>(&input_friends)->multitoken())
+     ("folder",        po::value<std::string>(&folder)->default_value(folder))
+     ("tree",          po::value<std::string>(&tree)->default_value(tree))
+     ("first_entry",   po::value<unsigned int>(&first_entry)->default_value(first_entry))
+     ("last_entry",    po::value<unsigned int>(&last_entry)->default_value(last_entry))
+     ("lwtnn_config",  po::value<std::string>(&lwtnn_config)->default_value(lwtnn_config));
+  po::store(po::command_line_parser(argc, argv).options(config).run(), vm);
+  po::notify(vm);
+
+  // Access input file and tree
+  auto in = TFile::Open(input.c_str(), "read");
+  auto dir = (TDirectoryFile *)in->Get(folder.c_str());
+  auto inputtree = (TTree *)dir->Get(tree.c_str());
+  for(std::vector<std::string>::const_iterator s = input_friends.begin(); s != input_friends.end(); ++s)
+  {
+    inputtree->AddFriend((folder+"/"+tree).c_str(), (&(*s))->c_str());
+  }
+
+  // Set up lwtnn
+  if (!boost::filesystem::exists(lwtnn_config)) {
+      throw std::runtime_error("LWTNN config file does not exist.");
+  }
+  std::ifstream config_file(lwtnn_config);
+  auto nnconfig = lwt::parse_json(config_file);
+  lwt::LightweightNeuralNetwork model(nnconfig.inputs, nnconfig.layers, nnconfig.outputs);
+
+  // Initialize inputs
+  std::map<std::string, Float_t> inputs;
+  for(size_t n=0; n < nnconfig.inputs.size(); n++)
+  {
+    inputs[nnconfig.inputs.at(n).name] = 0.0;
+    inputtree->SetBranchAddress((nnconfig.inputs.at(n).name).c_str(), &(inputs.find(nnconfig.inputs.at(n).name)->second));
+  }
+
+  // Initialize output file
+  auto outputname =
+      outputname_from_settings(input, folder, first_entry, last_entry);
+  boost::filesystem::create_directories(filename_from_inputpath(input));
+  auto out = TFile::Open(outputname.c_str(), "recreate");
+  out->mkdir(folder.c_str());
+  out->cd(folder.c_str());
+
+  // Create output tree
+  auto nnfriend = new TTree("ntuple", "NN score friend tree");
+
+  // Initialize outputs for the tree
+  std::map<std::string, Float_t> outputs;
+  for(size_t n=0; n < nnconfig.outputs.size(); n++)
+  {
+    outputs[nnconfig.outputs.at(n)] = 0.0;
+    nnfriend->Branch((nnconfig.outputs.at(n)).c_str(), &(outputs.find(nnconfig.outputs.at(n))->second), (nnconfig.outputs.at(n)+"/F").c_str());
+  }
+
+  // Loop over desired events of the input tree & compute outputs
+  for (unsigned int i = first_entry; i <= last_entry; i++) {
+    // Get entry
+    inputtree->GetEntry(i);
+
+    // Convert the inputs from Float_t to double
+    std::map<std::string, double> model_inputs;
+    for(auto &in : inputs)
+    {
+      model_inputs[in.first] = in.second;
+    }
+   
+    // Apply model on inputs
+    auto model_outputs = model.compute(model_inputs);
+
+    // Fill output map
+    for(const auto& output: model_outputs)
+    {
+      outputs[output.first] = output.second;
+    }
+
+    // Fill output tree
+    nnfriend->Fill();
+  }
+
+  // Fill output file
+  out->cd(folder.c_str());
+  nnfriend->Write("", TObject::kOverwrite);
+  out->Close();
+  in->Close();
+
+  return 0;
+}
